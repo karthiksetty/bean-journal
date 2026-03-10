ddadadec6243464157362c67444eae930709c29d
Karthik Setty <karthik.setty@gmail.com>
2026-03-10 11:08:16 +0100

    Remove .next from git, update .gitignore

--- .next/server/app/apple-icon/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/app/apple-icon/route.js")
-R.c("server/chunks/[root-of-the-server]__12ad1bb7._.js")
-R.c("server/chunks/[root-of-the-server]__db78f342._.js")
-R.c("server/chunks/_next-internal_server_app_apple-icon_route_actions_c01c3a02.js")
-R.m(55529)
-module.exports=R.m(55529).exports
